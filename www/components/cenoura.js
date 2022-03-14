803a4581901997003f28d937a9fca3c13e3c9398
GiovannaNO <giovanna10nunes@gmail.com>
2022-03-15 08:27:57 +0900

    funcionando

--- www/components/cenoura.js
+++ www/components/cenoura.js
@@ -2,13 +2,14 @@
 
 //Função que é executada no carregamento da página 
 window.onload = function(){
-    let A = parseInt(document.querySelector("#valor1").value);
-    let B = parseInt(document.querySelector("#valor2").value);
-    let C = parseInt(document.querySelector("#valor3").value);
-
+    
     let calc = document.querySelector("#calc");
-
+    
     calc.addEventListener("click", function(){
+        let A = parseInt(document.querySelector("#valor1").value);
+        let B = parseInt(document.querySelector("#valor2").value);
+        let C = parseInt(document.querySelector("#valor3").value);
+
         if((A<B+C) && (B<A+C) && (C<A+B)){
             if(A==B && B==C){
                 document.getElementById("result").value = "Equilátero! '-'";
