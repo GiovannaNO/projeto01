59f572ed7e18d88b75c1d1a9d7acddc49293e8c8
GiovannaNO <giovanna10nunes@gmail.com>
2022-03-15 00:26:43 +0900

    safsnai

--- www/components/cenoura.js
+++ www/components/cenoura.js
@@ -2,14 +2,23 @@
 
 //Função que é executada no carregamento da página 
 window.onload = function(){
-    const valor1 = document.querySelector("#valor1");
-    const valor2 = document.querySelector("#valor2");
-    const valor3 = document.querySelector("#valor3");
+    let valor1 = document.querySelector("#valor1");
+    let valor2 = document.querySelector("#valor2");
+    let valor3 = document.querySelector("#valor3");
 
-    const calc = document.querySelector("#calc");
+    let calc = document.querySelector("#calc");
 
     calc.addEventListener("click", function(){
-        let resultado = parseFloat(valor1.value) + parseFloat(valor2.value) + parseFloat(valor3.value);
-        document.querySelector("#result").value = resultado;
+        if((valor1.value < valor2.value + valor3.value) && (valor2.value < valor1.value + valor3.value) && (valor3.value < valor1.value + valor2.value)){
+            if((valor1.value == valor2.value)&& (valor2.value == valor3.value)&& (valor3.value == valor1.value)){
+                document.getElementById("result").value = "Equilátero! '-'";
+            } else if((valor1.value != valor2.value)&& (valor2.value != valor3.value)&& (valor3.value != valor1.value)){
+                document.getElementById("result").value = "Escaleno! '_'";
+            } else {
+                 document.getElementById("result").value = "Isóceles! 'O'";
+            }
+        } else{
+             document.getElementById("result").value = "Inválido";
+        }
     });
 }
